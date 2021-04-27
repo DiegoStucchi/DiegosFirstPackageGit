152428270465b8bbac22f59dfbf7b6835410f364
Diego Stucchi <diego.stucchi@polimi.it>
2021-04-27 17:50:42 +0200

    Add basic setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='awesomediegopackage',
+      version='0.0.1',
+      description='My first (useless) package',
+      url='https://github.com/DiegoStucchi/DiegosFirstPackageGit',
+      author='Diego Stucchi',
+      author_email='stucchidiego1994@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      keywords=['first package'])
