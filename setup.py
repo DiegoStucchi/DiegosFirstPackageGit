2f5bca1ba6a007e04db64faf724953628e70e29a
Diego Stucchi <diego.stucchi@polimi.it>
2021-04-27 18:25:27 +0200

    Change the setup.py file
    I added some more details to the package setup file to make it look like a serious package.

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 from setuptools import setup, find_packages
 
-setup(name='awesomediegopackage',
-      version='0.0.1',
-      description='My first (useless) package',
-      url='https://github.com/DiegoStucchi/DiegosFirstPackageGit',
-      author='Diego Stucchi',
-      author_email='stucchidiego1994@gmail.com',
-      license='MIT',
-      packages=find_packages(),
-      keywords=['first package'])
+VERSION = '0.0.1'
+DESCRIPTION = 'My first (useless) package'
+LONG_DESCRIPTION = 'My first (useless) package containing some basic (useless) functions that we can use to test whether it use installed correctly.'
+
+# Setting up
+setup(
+    name="awesomediegopackage",
+    version=VERSION,
+    author="Diego Stucchi (test)",
+    author_email="stucchidiego1994@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'first package'],
+    url='https://github.com/DiegoStucchi/DiegosFirstPackageGit',
+    license='MIT',
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
